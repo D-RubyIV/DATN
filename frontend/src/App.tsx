36b46f5eee7d26973157a18be51ae7d8ec42266c
04 DION <phamhaanh2k4.php@gmail.com>
2024-09-05 01:22:51 +0700

    200

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import HomePage from "./pages/HomePage"
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen">
+      <div>
+        <HomePage></HomePage>
+      </div>
+    </div>
+  )
+}
+export default App
