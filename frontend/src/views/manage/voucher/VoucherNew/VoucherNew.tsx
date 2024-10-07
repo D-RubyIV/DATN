bb2ebcfddbe21fc7dca30e8da236f460fe656216
ngochungsoftware <hunglnph45965@fpt.edu.vn>
2024-10-07 19:52:13 +0700

    fix fe

--- frontend/src/views/manage/voucher/VoucherNew/VoucherNew.tsx
+++ frontend/src/views/manage/voucher/VoucherNew/VoucherNew.tsx
@@ -1,8 +1,9 @@
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import VoucherForm, { FormModel, SetSubmitting } from "../VoucherForm"
 import axios from "axios"
 import toast from '@/components/ui/toast'
 import { Notification } from "@/components/ui"
+import { AdaptableCard } from "@/components/shared"
  
 const VoucherNew =() => {
 
@@ -41,9 +42,49 @@ const VoucherNew =() => {
         navigate('/manager/voucher/voucher-list')
     }
     return (
+
+        <AdaptableCard className="h-full" bodyClass="h-full">
+        {/* Breadcrumb */}
+        <div className="lg:flex items-center justify-between mb-4">
+            <nav className="flex" aria-label="Breadcrumb">
+                <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                    <li>
+                        <div className="flex items-center">
+                            <Link to="/" className="text-gray-700 hover:text-blue-600">
+                                Trang Chủ
+                            </Link>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="flex items-center">
+                            <span className="mx-2">/</span>
+                            <Link to="/manage" className="text-gray-700 hover:text-blue-600">
+                                Quản Lý
+                            </Link>
+                        </div>
+                    </li>
+                    <li aria-current="page">
+                        <div className="flex items-center">
+                            <span className="mx-2">/</span>
+                            <Link to="/manage/voucher" className="text-gray-700 hover:text-blue-600">
+                                Quản Lý
+                            </Link>
+                        </div>
+                    </li>
+                    <li aria-current="page">
+                        <div className="flex items-center">
+                            <span className="mx-2">/</span>
+                            <span className="text-gray-500">Thêm Phiếu Giảm Giá</span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+        </div>
         <>
             <VoucherForm type="new" onFormSubmit={handleFormSubmit} onDiscard={handleDiscard}/>
         </>
+    </AdaptableCard>
+       
     )
 }
 
