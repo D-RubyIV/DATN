2954991eb8b8f66e52ad704feb57b4f695ce5ad7
ngochungsoftware <hunglnph45965@fpt.edu.vn>
2024-10-14 17:59:30 +0700

    sort enum

--- backend/src/main/java/org/example/demo/entity/order/enums/Status.java
+++ backend/src/main/java/org/example/demo/entity/order/enums/Status.java
@@ -1,15 +1,15 @@
 package org.example.demo.entity.order.enums;
 
 public enum Status {
-    CREATE_AN_ORDER, // Tạo Đơn Hàng
     WAITING_YOUR_PAYMENT, // Chờ thanh toán
-    CONFIRM_PAYMENT, // Xác nhận thông tin thanh toán
-    EDIT_AN_ORDER, // Chỉnh sửa đơn hàng
-
+    CREATE_AN_ORDER, // Tạo Đơn Hàng
     PENDING,    // Chờ xác nhận
+    CONFIRM_PAYMENT, // Xác nhận thông tin thanh toán
     TOSHIP,     // Chờ giao hàng
     TORECEIVE,  // Đang giao hàng
     DELIVERED,  // Hoàn thành
     CANCELED,   // Đã hủy
-    RETURNED    // Trả hàng
+    RETURNED,    // Trả hàng
+    EDIT_AN_ORDER // Chỉnh sửa đơn hàng
+
 }
