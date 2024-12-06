8fa7e6e08362c0b0700af8e5625a2a801f554d49
ngochungsoftware <hunglnph45965@fpt.edu.vn>
2024-12-06 23:28:45 +0700

    done forgot pass fe

--- frontend/src/views/client/Popup/ForgotPasswordModal.tsx
+++ frontend/src/views/client/Popup/ForgotPasswordModal.tsx
@@ -1,35 +1,92 @@
-import React, { SetStateAction, useState } from 'react'
+import { useState } from 'react'
+import { FormItem, FormContainer } from '@/components/ui/Form'
+import Input from '@/components/ui/Input'
+import Button from '@/components/ui/Button'
+import Alert from '@/components/ui/Alert'
+import ActionLink from '@/components/shared/ActionLink'
+import { Field, Form, Formik } from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+import { toast, ToastContainer } from 'react-toastify'
 
 interface ForgotPasswordModalProps {
     isOpen: boolean;
-    setIsOpenLoginModal: React.Dispatch<SetStateAction<boolean>>
-    setIsOpenForgotModal: React.Dispatch<SetStateAction<boolean>>
+    setIsOpenLoginModal: React.Dispatch<React.SetStateAction<boolean>>
+    setIsOpenForgotModal: React.Dispatch<React.SetStateAction<boolean>>
     onClose: () => void;
 }
 
-const ForgotPasswordModal: React.FC<ForgotPasswordModalProps> = ({ isOpen,setIsOpenLoginModal, setIsOpenForgotModal, onClose }) => {
-    const [email, setEmail] = useState('');
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState(false);
-    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+type ForgotPasswordFormSchema = {
+    email: string
+    verificationCode: string
+    newPassword: string
+}
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setLoading(true);
-        setError(null);
-        setSuccessMessage(null);
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required('Please enter your email').email('Invalid email address'),
+    verificationCode: Yup.string().when('emailSent', {
+        is: true,
+        then: Yup.string().required('Please enter the verification code'),
+    }),
+    newPassword: Yup.string().when('emailSent', {
+        is: true,
+        then: Yup.string().required('Please enter your new password').min(6, 'Password must be at least 6 characters'),
+    }),
+})
 
+const ForgotPasswordModal: React.FC<ForgotPasswordModalProps> = ({ 
+    isOpen, 
+    setIsOpenLoginModal, 
+    setIsOpenForgotModal, 
+    onClose 
+}) => {
+    const [emailSent, setEmailSent] = useState(false)
+    const [message, setMessage] = useState<string | null>(null)
+    const [showPasswordForm, setShowPasswordForm] = useState(false)
+
+    const onSendMail = async (
+        values: ForgotPasswordFormSchema,
+        setSubmitting: (isSubmitting: boolean) => void
+    ) => {
+        setSubmitting(true)
         try {
-            await new Promise((resolve) => setTimeout(resolve, 1500)); 
-            setSuccessMessage('Yêu cầu đặt lại mật khẩu đã được gửi. Vui lòng kiểm tra email của bạn!');
-            setIsOpenForgotModal(false)
-            setIsOpenLoginModal(true)
-        } catch (err) {
-            setError('Không thể gửi yêu cầu. Vui lòng thử lại.');
-        } finally {
-            setLoading(false);
+            const response = await axios.post('http://localhost:8080/api/v1/users/request-verification-code', {
+                email: values.email,
+            })
+            if (response.status === 200) {
+                setEmailSent(true)
+                setShowPasswordForm(true)
+                toast.success('Mã xác minh đã được gửi đến email của bạn')
+            }
+        } catch (error: any) {
+            const errorMessage = error.response?.data?.message || error.message || 'Có lỗi xảy ra!'
+            setMessage(errorMessage)
+            toast.error(errorMessage)
         }
-    };
+        setSubmitting(false)
+    }
+
+    const onChangePassword = async (
+        values: ForgotPasswordFormSchema,
+        setSubmitting: (isSubmitting: boolean) => void
+    ) => {
+        setSubmitting(true)
+        try {
+            const response = await axios.post('http://localhost:8080/api/v1/users/reset-password/v2', {
+                email: values.email,
+                verificationCode: values.verificationCode,
+                newPassword: values.newPassword,
+            })
+            if (response.status === 200) {
+                toast.success('Mật khẩu của bạn đã được đặt lại thành công')
+            }
+        } catch (error: any) {
+            const errorMessage = error.response?.data?.message || error.message || 'Có lỗi xảy ra!'
+            setMessage(errorMessage)
+            toast.error(errorMessage)
+        }
+        setSubmitting(false)
+    }
 
     if (!isOpen) return null;
 
@@ -59,57 +116,99 @@ const ForgotPasswordModal: React.FC<ForgotPasswordModalProps> = ({ isOpen,setIsO
                     </button>
 
                     <div className="p-5">
-                        <div className="text-center">
-                            <p className="mb-3 text-2xl font-semibold leading-5 text-slate-900">
-                                Quên Mật Khẩu
-                            </p>
-                            <p className="mt-2 text-sm leading-4 text-slate-600">
-                                Vui lòng nhập email của bạn để nhận liên kết đặt lại mật khẩu.
-                            </p>
+                        <ToastContainer />
+                        <div className="mb-6">
+                            {emailSent ? (
+                                <>
+                                    <h3 className="mb-1">Kiểm tra email của bạn</h3>
+                                    <p>Chúng tôi đã gửi hướng dẫn khôi phục mật khẩu tới email của bạn</p>
+                                </>
+                            ) : (
+                                <>
+                                    <h3 className="mb-1">Quên mật khẩu</h3>
+                                    <p>Vui lòng nhập địa chỉ email của bạn để nhận mã xác minh</p>
+                                </>
+                            )}
                         </div>
 
-                        <form className="w-full mt-4" onSubmit={handleSubmit}>
-                            <label htmlFor="email" className="sr-only">
-                                Địa chỉ email
-                            </label>
-                            <input
-                                required
-                                name="email"
-                                type="email"
-                                className="block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-black"
-                                placeholder="Email Address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
+                        {message && (
+                            <Alert showIcon className="mb-4" type="danger">
+                                {message}
+                            </Alert>
+                        )}
 
-                            <button
-                                type="submit"
-                                className="inline-flex w-full items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white mt-4"
-                                disabled={loading}
-                            >
-                                {loading ? 'Processing...' : 'Gửi yêu cầu'}
-                            </button>
-                        </form>
+                        <Formik
+                            initialValues={{ email: '', verificationCode: '', newPassword: '' }}
+                            validationSchema={validationSchema}
+                            onSubmit={(values, { setSubmitting }) => {
+                                if (!emailSent) {
+                                    onSendMail(values, setSubmitting)
+                                } else if (showPasswordForm) {
+                                    onChangePassword(values, setSubmitting)
+                                } else {
+                                    setSubmitting(false)
+                                }
+                            }}
+                        >
+                            {({ touched, errors, isSubmitting }) => (
+                                <Form>
+                                    <FormContainer>
+                                        {/* Email Field */}
+                                        {!emailSent && !showPasswordForm && (
+                                            <FormItem invalid={errors.email && touched.email} errorMessage={errors.email}>
+                                                <Field
+                                                    type="email"
+                                                    autoComplete="off"
+                                                    name="email"
+                                                    placeholder="Email"
+                                                    component={Input}
+                                                />
+                                            </FormItem>
+                                        )}
 
-                        {error && <p className="mt-2 text-red-500">{error}</p>}
-                        {successMessage && <p className="mt-2 text-green-500">{successMessage}</p>}
+                                        {/* Verification Code and New Password Fields */}
+                                        {showPasswordForm && (
+                                            <>
+                                                <FormItem invalid={errors.verificationCode && touched.verificationCode} errorMessage={errors.verificationCode}>
+                                                    <Field
+                                                        type="text"
+                                                        name="verificationCode"
+                                                        placeholder="Verification Code"
+                                                        component={Input}
+                                                    />
+                                                </FormItem>
+                                                <FormItem invalid={errors.newPassword && touched.newPassword} errorMessage={errors.newPassword}>
+                                                    <Field
+                                                        type="password"
+                                                        name="newPassword"
+                                                        placeholder="New Password"
+                                                        component={Input}
+                                                    />
+                                                </FormItem>
+                                            </>
+                                        )}
 
-                        <div className="mt-6 text-center text-sm text-slate-600">
-                            <button
-                                className="font-medium text-[#4285f4]"
-                                onClick={() => {
-                                    setIsOpenForgotModal(false)
-                                    setIsOpenLoginModal(true)
-                                }}
-                            >
-                                Quay lại đăng nhập
-                            </button>
-                        </div>
+                                        <Button block loading={isSubmitting} variant="solid" type="submit">
+                                            {emailSent ? 'Xác nhận' : 'Gửi Email'}
+                                        </Button>
+                                        <div className="mt-4 text-center">
+                                            <span>Quay lại </span>
+                                            <ActionLink onClick={() => {
+                                                setIsOpenForgotModal(false)
+                                                setIsOpenLoginModal(true)
+                                            }}>
+                                                Đăng nhập
+                                            </ActionLink>
+                                        </div>
+                                    </FormContainer>
+                                </Form>
+                            )}
+                        </Formik>
                     </div>
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default ForgotPasswordModal;
+export default ForgotPasswordModal
